77bd532d8de4361a66388dc75314e4c862a072bb
gnol-on-the-beat <krille.westerlund@gmail.com>
2023-10-27 23:01:01 +0200

    Update control.gd
    njhgfds

--- bil test/art/control.gd
+++ bil test/art/control.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 # Yo what tha fast is up
-# Pimps and playas
+# simps
 
 var turbo_sprite = preload("res://art/car_turbo.png")
 var car_sprite = preload("res://art/car.png")
